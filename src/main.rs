9f4d2405e5b9b52f124b08931eb906eb808d8c42
Mrnarwhal109 <jediknight1091@gmail.com>
2023-02-18 17:00:07 -0600

    Project renamed.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,8 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
+use gv_server::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-    })
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
-}
-
-// Returns something that implements the Responder trait.
-// A type implements the Responder trait if it can be converted into a HttpResponse.
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+    // Bubble up the io::Error if we failed to bind the address
+    // Otherwise call .await on our server
+    run()?.await
 }
