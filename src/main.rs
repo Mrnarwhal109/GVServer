e89da9f2c2e978853b7940108b496527fcc3ad6b
Mrnarwhal109 <jediknight1091@gmail.com>
2023-03-14 13:31:18 -0500

    Changed base config for testing.

--- src/main.rs
+++ src/main.rs
@@ -33,10 +33,7 @@ async fn main() -> std::io::Result<()> {
     // No longer async, given that we don't actually try to connect!
     let connection_pool = PgPoolOptions::new()
         .acquire_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy(
-        &configuration.database.connection_string().expose_secret()
-    )
-        .expect("Failed to create Postgres connection pool.");
+        .connect_lazy_with(configuration.database.with_db());
 
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our server
